b8177b43f341874c2bf6274e9ce5ae84466150ae
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2022-03-11 04:46:33 -0300

    promises -> async/await

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,28 +3,29 @@ const router = express.Router();
 const { getCompetitions, getCompetition, getCompetitionRanking } = require('../services/competitionService')
 const { registerDonation } = require('../services/donationService')
 
-router.get("/", (req, res, next) => {
-  getCompetitions().then((competitions) => {
+router.get("/", async (req, res, next) => {
+  try {
+    const competitions = await getCompetitions()
     res.status(200).json(competitions);
-  })
-  .catch((err) => {
+  } catch (err) {
     res.status(500).json({ "message": "Ocorreu um erro inesperado, desculpe pelo transtorno."}) 
     console.log(err)
-  });
+  }
 });
 
-router.get("/:id/ranking", (req, res, next) => {
+router.get("/:id/ranking", async (req, res, next) => {
   const id = parseInt(req.params.id)
   if (!Number.isInteger(id)) {
     return res.status(404).json({ "message": "Competição não encontrada." })
   }
 
-  getCompetitionRanking(id).then((ranking) => {
+  try {
+    const ranking = await getCompetitionRanking(id)
     res.status(200).json(ranking)
-  }).catch((err) => {
+  } catch (err) {
     res.status(404).json({ "message": "Competição não encontrada."}) 
     console.log(err)
-  });
+  }
 });
 
 router.post('/:id/donations', (req, res, next) => {
@@ -35,24 +36,25 @@ router.post('/:id/donations', (req, res, next) => {
 
   const { user_name, user_email, competitionTeamId } = req.body;
 
-  getCompetition(id)
-    .then((competition) => {
-      if (competition === null) {
-        return res.status(404).json({ message: "Competição não encontrada." })
-      } else if (competition.dataValues.status != 2) {
-        return res.status(422).json({ message: "Esta competição não está disponível para registro de doações."})
-      } else {
-        registerDonation(competitionId, competitionTeamId, user_name, user_email)
-        .then((donation) => {
-          return res.status(201).json(donation)
-        }).catch((err) => {
-          console.log(`Erro ${err} quando registrando doação.`)
-        })
+  try {
+    const competition = getCompetition(id)
+    if (competition === null) {
+      return res.status(404).json({ message: "Competição não encontrada." })
+    } else if (competition.dataValues.status != 2) {
+      return res.status(422).json({ message: "Esta competição não está disponível para registro de doações."})
+    } else {
+      console.log('retornado')
+      try {
+        const donation = registerDonation(competitionId, competitionTeamId, user_name, user_email)
+        return res.status(201).json(donation)
+      } catch(err) {
+        console.log(`Erro ${err} quando registrando doação.`)
       }
-    })
-    .catch((err) => {
-      next(err);
-    });
-})
+    }
+    } catch (err) {
+      next(err)
+    }
+  }
+)
 
 module.exports = { url: "/competitions", router };
