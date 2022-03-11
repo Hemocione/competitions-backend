0c1169b8904b53dbcf3719361d3b3fc602b6b453
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2022-03-11 05:41:53 -0300

    change config to use DATABASE_URL

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
-module.exports = {
-  development: {
+module.exports = { 
+  local: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
@@ -14,11 +14,18 @@ module.exports = {
       }
     }
   },
+  development: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: "postgres",
+    "dialectOptions": {
+      "ssl": {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    use_env_variable: 'DATABASE_URL',
     dialect: "postgres",
     "dialectOptions": {
       "ssl": {
