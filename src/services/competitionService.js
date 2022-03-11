0aee8ae96d6f1191511999116870fd1b46561cfa
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2022-03-11 03:19:17 -0300

    finish competition ranking detail route

--- src/services/competitionService.js
+++ src/services/competitionService.js
@@ -1,14 +1,15 @@
 const { competition, sequelize, competitionTeam, team } = require('../db/models');
 
-const getCompetitions = async () => {
-  // status 2 = ativo, 1 = upcoming, 0 = finalizado
-  const statusCaseWhenClause = `
-    CASE
-      WHEN CURRENT_TIMESTAMP > end_at THEN 0
-      WHEN CURRENT_TIMESTAMP < start_at THEN 1
-      ELSE 2
-    END`
+// status 2 = ativo, 1 = upcoming, 0 = finalizado
+const statusCaseWhenClause = `
+CASE
+  WHEN CURRENT_TIMESTAMP > end_at THEN 0
+  WHEN CURRENT_TIMESTAMP < start_at THEN 1
+  ELSE 2
+END`
 
+
+const getCompetitions = async () => {
   return (
     await competition.findAll({
       attributes: [
@@ -26,19 +27,28 @@ const getCompetitions = async () => {
 const getCompetition = async (id) => {
   return (
     await competition.findOne({
+      attributes: [
+        'id', 'name',
+        [sequelize.literal(statusCaseWhenClause), 'status']
+      ],
       where: { 
         id: id
       },
       include: [
         {
           model: competitionTeam,
+          attributes: ['donation_count'],
           include: [
             {
-              model: team
+              model: team,
+              attributes: ['name']
             }
-          ]
+          ],
         }
       ],
+      order: [
+        [{ model: competitionTeam }, 'donation_count', 'desc']
+      ]
     })
   )
 }
