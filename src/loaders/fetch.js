412b3d304260e224582ce6a55374f793043a8c23
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2022-03-15 20:59:06 -0300

    downgrade node-fetch and add loader

---
+++ src/loaders/fetch.js
@@ -0,0 +1,9 @@
+const fetch = require('node-fetch');
+
+const loader = () => {
+  if (!globalThis.fetch) {
+    globalThis.fetch = fetch
+  }
+}
+
+module.exports = loader;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,3 @@
-const fetch = require('node-fetch');
 const express = require("express");
 const router = express.Router();
 const { getCompetitions, getCompetition, getCompetitionRanking } = require('../services/competitionService')
