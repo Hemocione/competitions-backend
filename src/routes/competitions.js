af0b69c0a96a513610757933d5653c96d775b44e
Lucas <lucas@ecoding.com.br>
2022-04-03 06:00:54 -0300

    add endpoint to get institutions rank by id

--- src/routes/competitions.js
+++ src/routes/competitions.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { getCompetitions, getCompetition, getCompetitionRanking } = require('../services/competitionService')
+const { getRankingByCompetitionId } = require('../services/institutionService')
 const NotFoundError = require('../errors/NotFoundError');
 
 router.get("/competitions/:id/rankings", async(req, res, next) => {
-    //const id = parseInt(req.params.id);
-    next(new NotFoundError("Competition not Found!"));
+    const id = parseInt(req.params.id);
+    if (!Number.isInteger(id))
+        return next(new NotFoundError("Competition not Found!"));
+
+    const result = await getRankingByCompetitionId(id);
+    res.status(200).json(result);
 });
 
 module.exports = { url: "/v1", router };
