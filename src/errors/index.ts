11d3b6c35f7f84ccd1bc3559750f48eae75260f4
matheusleo <matheusleotrab@gmail.com>
2022-09-20 21:27:10 -0300

    Revert "Revert "Merge pull request #6 from Hemocione/develop""
    This reverts commit e6c39c071db0408a9c85794d8bf6d229fc6941f6.

---
+++ src/errors/index.ts
@@ -0,0 +1,29 @@
+export class CompetitionError extends Error {
+  public status
+
+  constructor(message: string, status: number) {
+    super(message)
+    this.status = status
+  }
+}
+
+export class NotFoundError extends CompetitionError {
+  constructor(message: string) {
+    const statusCode = 404
+    super(message, statusCode)
+  }
+}
+
+export class Unauthorized extends CompetitionError {
+  constructor(message: string) {
+    const statusCode = 401
+    super(message, statusCode)
+  }
+}
+
+export class Unexpected extends CompetitionError {
+  constructor(message: string) {
+    const statusCode = 500
+    super(message, statusCode)
+  }
+}
