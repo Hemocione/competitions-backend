05777d732098f64f8cf1a9206a4dd761f4af5083
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2023-10-31 21:53:57 -0300

    use npm only and hotfix some loading shit and better dev env

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,22 @@
-import express from 'express'
-import loader from './loaders'
-import dotenv from 'dotenv'
-const app = express()
-dotenv.config()
-const PORT = process.env.PORT || process.env.SERVER_PORT || 8080
+import express from "express";
+import loader from "./loaders";
+import dotenv from "dotenv";
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || process.env.SERVER_PORT || 8080;
 
 const end = loader({
   expressApp: app,
-  isTest: process.env.ENV === 'test' ? true : false,
+  isTest: process.env.ENV === "test" ? true : false,
 })
   .then(() => {
-    return app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`))
+    console.log("Loader Finished");
+    return app.listen(PORT, () =>
+      console.log(`Server Running on Port ${PORT}`)
+    );
   })
   .catch((err) => {
-    console.log(`Loader Failed err: ${err}`)
-  })
+    console.log(`Loader Failed err: ${err}`);
+  });
 
-export default { server: app, end }
+export default { server: app, end };
